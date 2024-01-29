19e3fa8fb51eb2eee47b26062a9a611c8f717165
kidMax25 <snmaxis@gmail.com>
2024-01-29 09:05:07 +0300

    The for-Loop

---
+++ Control Structures/For-Loop.cpp
@@ -0,0 +1,14 @@
+// The for Loop
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 10; i > 0; i--)
+    {
+        cout << i << ",";
+    }
+    cout << "Fire/n";
+    return 0;
+}
